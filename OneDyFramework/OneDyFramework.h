264c06863e028a16f162bfbcbf85ebe862c167b2
Iswandi Saputra <iswandi.saputra11@gmail.com>
2020-10-22 21:14:27 +0700

    Initial Commit

---
+++ OneDyFramework/OneDyFramework.h
@@ -0,0 +1,19 @@
+//
+//  OneDyFramework.h
+//  OneDyFramework
+//
+//  Created by Iswandi Saputra on 22/10/20.
+//  Copyright © 2020 inatechno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneDyFramework.
+FOUNDATION_EXPORT double OneDyFrameworkVersionNumber;
+
+//! Project version string for OneDyFramework.
+FOUNDATION_EXPORT const unsigned char OneDyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneDyFramework/PublicHeader.h>
+
+
